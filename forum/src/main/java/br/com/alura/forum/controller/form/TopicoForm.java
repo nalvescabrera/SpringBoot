852d736b1c5426ecb950b2afc1e760bee658e0b1
Natalia Cabrera <n.alvescabrera@gmail.com>
2021-07-23 22:08:46 -0300

    Validação com Bean Validation

--- forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -3,11 +3,18 @@ package br.com.alura.forum.controller.form;
 import br.com.alura.forum.controller.form.repository.CursoRepository;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import com.sun.istack.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
 
 public class TopicoForm {
 
+    @NotNull @NotEmpty @Length(min = 5)
     private String titulo;
+    @NotNull @NotEmpty @Length(min = 10)
     private String mensagem;
+    @NotNull @NotEmpty
     private String nomeCurso;
 
     public String getTitulo() {
